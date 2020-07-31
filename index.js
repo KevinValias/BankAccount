5db777cbb6d648cde242e4bb054f8d021db8861c
ArielHolman <ArielARodrigues01@gmail.com>
2020-07-31 08:47:04 -0500

    added all tests and passed them, created last savings class and tested, also updated all functions to fit the test.

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-undef */
+/* eslint-disable spaced-comment */
 const assert = require("assert")
-// const readline = require("linebyline");
-// const { isDate } = require("util");
-// const { type } = require("os");
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// })
 
 class Transaction {
   constructor(amount, payee) {
@@ -18,80 +14,151 @@ class BankAccount {
   constructor(accountNumber, owner) {
     this.accountNumber = accountNumber
     this.owner = owner
-    this.transaction = []
+    this.transactions = []
   }
-//should loops over all transactions and return the balance /sum of everything including negatives. 
 
   balance() {
-    function sumTotal(total, num) {
-      return total + num
+    let total = 0
+    for (let i = 0; i < this.transactions.length; i++) {
+      total += this.transactions[i].amount
     }
-    this.balance = this.transaction.reduce(sumTotal)
+    return total
   }
 
-  deposit(amt, payee) {
+  deposit(amt) {
     if (amt > 0) {
-      this.transaction.push(new Transaction(amt, payee))
-      this.balance = amt
-    } else {
-      return "Input a positive integer, please."
+      const depositTransaction = new Transaction(amt, "Deposit")
+      this.transactions.push(depositTransaction)
     }
   }
 
   charge(payee, amt) {
-    // balance = amt
-    if (this.balance - amt > 0) {
-      this.transaction.push(new Transaction(amt, payee))
-    } else if (this.balance - amt < 0) {
-      console.log("Ya broke, dude...")
+    const currentBalace = this.balance()
+    if (amt <= currentBalace) {
+      const chargeTransaction = new Transaction(-amt, payee)
+      this.transactions.push(chargeTransaction)
     }
   }
 }
+class SavingsAccount extends BankAccount {
+  constructor(accountNumber, owner, interestRate) {
+    super(accountNumber, owner)
+    this.interestRate = interestRate
+  }
 
-// const getPrompt = () => {
-//   printStacks()
-//   rl.question("start stack: ", (startStack) => {
-//     rl.question("end stack: ", (endStack) => {
-//       towersOfHanoi(startStack, endStack)
-//       getPrompt()
-//     })
-//   })
-// }
+  accrueInterest() {
+    const currentBalance = this.balance()
+    const interestAmt = currentBalance * this.interestRate
+    const interestTransaction = new Transaction(interestAmt, "Interest")
+    this.transactions.push(interestTransaction)
+  }
+}
 
+//TESTS
 if (typeof describe === "function") {
   describe("BankAccount", function () {
     it("should create Bank account: account number, name, transaction", function () {
-      const bankAccount1 = new BankAccount("900-300-200", "Bob Dylan")
-      assert.equal(bankAccount1.accountNumber, "900-300-200")
+      const bankAccount1 = new BankAccount("xx2345", "Bob Dylan")
       assert.equal(bankAccount1.owner, "Bob Dylan")
-      assert.equal(bankAccount1.transaction.length, 0)
+      assert.equal(bankAccount1.accountNumber, "xx2345")
+      assert.equal(bankAccount1.transactions.length, 0)
+      assert.equal(bankAccount1.balance(), 0)
     })
-    it("should deposit correctly", function () {
-      const arielsBank = new BankAccount("900-300-200", "Ariel")
-      arielsBank.deposit(1, "Flounder")
-      assert.equal(arielsBank.transaction[0].amount, 1)
-      assert.equal(arielsBank.transaction[0].payee, "Flounder")
-      arielsBank.deposit(10, "Sebastian")
-      assert.equal(arielsBank.transaction[1].amount, 10)
-      assert.equal(arielsBank.transaction[1].payee, "Sebastian")
+    it("Should return correct balance", function () {
+      const bankAccount1 = new BankAccount("xx2345", "Bob Dylan")
+      assert.equal(bankAccount1.balance(), 0)
+      bankAccount1.deposit(100)
+      assert.equal(bankAccount1.balance(), 100)
+      bankAccount1.charge("Starbucks", 10)
+      assert.equal(bankAccount1.balance(), 90)
+    })
+    it("Should not allow negative deposit", function () {
+      const bankAccount1 = new BankAccount("xx2345", "Bob Dylan")
+      assert.equal(bankAccount1.balance(), 0)
+      bankAccount1.deposit(100)
+      assert.equal(bankAccount1.balance(), 100)
+      bankAccount1.deposit(-30)
+      assert.equal(bankAccount1.balance(), 100)
+    })
+    it("Should prevent overdraft", function () {
+      const bankAccount1 = new BankAccount("xx2345", "Bob Dylan")
+      assert.equal(bankAccount1.balance(), 0)
+      bankAccount1.charge("Wayfair", 45)
+      assert.equal(bankAccount1.balance(), 0)
+    })
+    it("Should allow a refund", function () {
+      const bankAccount1 = new BankAccount("xx2345", "Bob Dylan")
+      assert.equal(bankAccount1.balance(), 0)
+      bankAccount1.charge("Home Depot", -65)
+      assert.equal(bankAccount1.balance(), 65)
     })
   })
-
   describe("Transaction", function () {
-    it("Should have date, payee, amount", function () {
+    it("Should create transactions with a date, payee, amount for a deposit", function () {
       const transaction1 = new Transaction(50, "Barb Dylan")
       assert.equal(transaction1.amount, 50)
       assert.equal(transaction1.payee, "Barb Dylan")
       assert.notEqual(transaction1.date, undefined)
       assert.notEqual(transaction1.date, null)
     })
+    it("Should create transactions with a date, payee, amount for a charge", function () {
+      const transaction1 = new Transaction(-41.65, "Target")
+      assert.equal(transaction1.amount, -41.65)
+      assert.equal(transaction1.payee, "Target")
+      assert.notEqual(transaction1.date, undefined)
+      assert.notEqual(transaction1.date, null)
+    })
+  })
+  describe("Multiple Transactions", function () {
+    const bankAccount3 = new BankAccount("xx4127", "Moana")
+    it("should create new account correctly", function () {
+      assert.equal(bankAccount3.owner, "Moana")
+      assert.equal(bankAccount3.accountNumber, "xx4127")
+      assert.equal(bankAccount3.balance(), 0)
+    })
+    it("should allow you to deposit multiple times and clearout account", function () {
+      bankAccount3.deposit(30)
+      bankAccount3.deposit(20)
+      bankAccount3.deposit(-3)
+      bankAccount3.deposit(34.25)
+      bankAccount3.deposit(10000.45)
+      assert.equal(10084.7, bankAccount3.balance())
+      bankAccount3.charge("Clearout", 10084.7)
+      assert.equal(0, bankAccount3.balance())
+    })
+    it("should balance account with multiple charges and count transactions", function () {
+      bankAccount3.deposit(10000)
+      bankAccount3.charge("target", 40)
+      bankAccount3.charge("chipotle", 10.32)
+      bankAccount3.charge("HEB", 40)
+      bankAccount3.charge("Bob's", -20)
+      assert.equal(9929.68, bankAccount3.balance())
+      assert.equal(10, bankAccount3.transactions.length)
+    })
+    it("should not allow overdraft", function () {
+      bankAccount3.charge("target", 40000)
+      assert.equal(9929.68, bankAccount3.balance())
+      assert.equal(10, bankAccount3.transactions.length)
+    })
+  })
+  describe("Multiple Transactions", function () {
+    it("should accrue interest correctly for deposit and create an account", function () {
+      const savingsMan = new SavingsAccount("xxx1010", "Aqua Man", 0.1)
+      assert.equal("xxx1010", savingsMan.accountNumber)
+      assert.equal("Aqua Man", savingsMan.owner)
+      assert.equal(0.1, savingsMan.interestRate)
+      assert.equal(0, savingsMan.balance())
+      savingsMan.deposit(100)
+      savingsMan.accrueInterest()
+      assert.equal(110, savingsMan.balance())
+    })
+    it("should accrue interest correctly for charge, account never reaches 0", function () {
+      const savingsMan = new SavingsAccount("xxx1010", "Aqua Man", 0.1)
+      savingsMan.deposit(100)
+      savingsMan.charge("ATM", 30)
+      savingsMan.accrueInterest()
+      assert.equal(77, savingsMan.balance())
+      assert.equal(true, savingsMan.balance() > 0)
+    })
   })
 }
-
-// Tests for your Bank Account App:
-// 4. Should deduct/charge correctly
-// 5. Should return correct balance
-// 6. Should prevent overdraft
-// 7. Should not allow negative deposit
-// 8. Should track multiple deposits and return accurate balance
-// 9. Also, 8. should track multiple charges and return accurate balance
